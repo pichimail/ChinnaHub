d59cdcfdd79d2e005a62e09acb7106fa7f96460b
pichimail <pichimail24@gmail.com>
2026-06-05 04:44:09 +0530

    fix(audio): simplify workspace to generation feed

--- src/routes/(main)/(create)/audio/index.tsx
+++ src/routes/(main)/(create)/audio/index.tsx
@@ -1,27 +1,14 @@
 'use client';
 
-import { Flexbox } from '@lobehub/ui';
 import { memo } from 'react';
 
 import CreateGenerationPage from '@/routes/(main)/(create)/features/CreateGenerationPage';
 
-import AlbumWorkspace from './features/AlbumWorkspace';
-import { AudioConversationPanel } from './features/AudioConversationPanel';
-import { AudioConversationReadyWatcher } from './features/AudioConversationPanel/ReadyWatcher';
-import PromptInput from './features/PromptInput';
-
-const ConversationalAudioWorkspace = memo(() => (
-  <Flexbox gap={16} width="100%">
-    <AudioConversationReadyWatcher />
-    <AudioConversationPanel />
-    <AlbumWorkspace />
-  </Flexbox>
-));
-
-ConversationalAudioWorkspace.displayName = 'ConversationalAudioWorkspace';
+import AudioWorkspace from './features/SimpleAudioWorkspace';
+import PromptInput from './features/PromptInput/Lean';
 
 const DesktopAudioPage = memo(() => (
-  <CreateGenerationPage PromptInput={PromptInput} Workspace={ConversationalAudioWorkspace} />
+  <CreateGenerationPage PromptInput={PromptInput} Workspace={AudioWorkspace} />
 ));
 
 DesktopAudioPage.displayName = 'DesktopAudioPage';
