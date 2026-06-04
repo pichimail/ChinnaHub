0a3a4e25297d20a6e6171f0a1dab7b101d16c482
pichimail <pichimail24@gmail.com>
2026-06-04 19:51:28 +0530

    feat(audio): export conversation selectors

--- src/store/audio/selectors.ts
+++ src/store/audio/selectors.ts
@@ -1,4 +1,5 @@
 export { createAudioSelectors } from './slices/createAudio/selectors';
+export { audioConversationSelectors } from './slices/conversation/selectors';
 export { audioGenerationBatchSelectors } from './slices/generationBatch/selectors';
 export { audioGenerationConfigSelectors } from './slices/generationConfig/selectors';
 export { audioGenerationTopicSelectors } from './slices/generationTopic/selectors';
