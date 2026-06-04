11aed21d176f9697d4d0ff6249d6ed11156a41ec
pichimail <pichimail24@gmail.com>
2026-06-05 04:46:30 +0530

    fix audio page imports

--- src/routes/(main)/(create)/audio/index.tsx
+++ src/routes/(main)/(create)/audio/index.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import { Flexbox } from '@lobehub/ui';
 import { memo } from 'react';
 
 import CreateGenerationPage from '@/routes/(main)/(create)/features/CreateGenerationPage';
 
-import AudioWorkspace from './features/SimpleAudioWorkspace';
+import AlbumWorkspace from './features/AlbumWorkspace';
 import PromptInput from './features/PromptInput/Lean';
+
+const AudioWorkspace = memo(() => (
+  <Flexbox gap={16} width="100%">
+    <AlbumWorkspace />
+  </Flexbox>
+));
+
+AudioWorkspace.displayName = 'AudioWorkspace';
 
 const DesktopAudioPage = memo(() => (
   <CreateGenerationPage PromptInput={PromptInput} Workspace={AudioWorkspace} />
